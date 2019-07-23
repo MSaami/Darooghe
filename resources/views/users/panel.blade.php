51c489b92486e8d2ad13c12bebbc86b306d56630
Mehrdad Saami <MehrdadSaami@gmail.com>
2019-07-23 17:55:28 +0430

    ready

--- resources/views/users/panel.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="card" style="width: 18rem;">
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item"><a href=" {{route('users.index')}} ">@lang('users.show users')</a></li>
-    <li class="list-group-item"><a href=" {{route('roles.index')}} ">@lang('users.show roles')</a></li>
-    <li class="list-group-item"> <a href=" {{route('permissions.index')}} "> @lang('users.add permission') </a> </li>
-  </ul>
-</div>
